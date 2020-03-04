32c46a3bb013906849290e7836ca18ec5dd50a6d
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-03-04 10:27:12 -0700

    Finalize links to other files

--- server.js
+++ server.js
@@ -4,53 +4,56 @@ const uuid = require("uuid").v4;
 
 const fs = require("fs");
 const path = require("path");
-const OUTPUT_DIR = path.resolve(__dirname, "././db");
+const OUTPUT_DIR = path.resolve(__dirname, "db");
 const outputPath = path.join(OUTPUT_DIR, "db.json");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 5050;
 
-// express data parsing
+// middleware for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// middleware to specify a static directory of 'public'
+app.use(express.static("public"));
 
 let notes = [];
 
 // ---------------------  FUNCTIONS --------------------------
 // self-calling function to get data from db file and add to local array
 (function readDbFile() {
-    fs.readFile(
-        "/Users/brittanyfortner/Desktop/Code/Homework/note-taker/db/db.json",
-        function(err, data) {
-            if (err) {
-                console.log(err);
-            } else {
-                notes = JSON.parse(data);
-            }
+    fs.readFile(outputPath, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            notes = JSON.parse(data);
         }
-    );
+    });
 })();
 
 // function to write new db file with note additions and deletions
 function writeFile() {
     let overwrite = JSON.stringify(notes);
-    fs.writeFile(
-        "/Users/brittanyfortner/Desktop/Code/Homework/note-taker/db/db.json",
-        overwrite,
-        function(err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("File written");
-            }
+    fs.writeFile(outputPath, overwrite, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("File written");
         }
-    );
+    });
 }
 
 // ------------------------- ROUTES ---------------------------
-// basic route to notes HTML file
+
+// basic routes to notes HTML files
+app.get("/", function(req, res) {
+    const indexPath = path.join(__dirname, "/public/index.html");
+    res.sendFile(indexPath);
+});
+
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    const notesPath = path.join(__dirname, "/public/notes.html");
+    res.sendFile(notesPath);
 });
 
 // respond with all notes
@@ -66,19 +69,6 @@ app.post("/api/notes", function(req, res) {
     notes.push(newNote);
     writeFile();
     return res.json(newNote);
-});
-
-// displays a single note
-app.get("/api/notes/:note", function(req, res) {
-    let chosen = req.params.note;
-    console.log(chosen);
-
-    for (let i = 0; i < notes.length; i++) {
-        if (chosen === notes[i].id) {
-            return res.json(notes[i]);
-        }
-    }
-    return res.json(false);
 });
 
 // delete selected note
@@ -98,7 +88,8 @@ app.delete("/api/notes/:id", function(req, res) {
 
 // basic route to index HTML file
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    const indexPath = path.join(__dirname, "/public/index.html");
+    res.sendFile(indexPath);
 });
 
 // starts the server to begin listening
